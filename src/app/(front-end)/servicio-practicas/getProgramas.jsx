6d6e04fa3ad623c6d9dbe67ceabda6060fa1244c
Ricardo Farid Perez Coyote <ti44793@uvp.edu.mx>
2025-03-14 02:11:05 -0600

    Hooks working

--- src/app/(front-end)/servicio-practicas/getProgramas.jsx
+++ src/app/(front-end)/servicio-practicas/getProgramas.jsx
@@ -1,23 +1,12 @@
-import { getPayload } from 'payload'
-import config from '@payload-config'
-import ServicioPracticas from './Client'
-import { unstable_cache } from 'next/cache'
-
-const getCachedPracticas = unstable_cache(
-  async () => {
-    const payload = await getPayload({ config })
-    const data = await payload.find({
-      collection: 'practicas-servicio',
-      limit: 100,
-      sort: 'id'
-    })
-    return data.docs // Retorna solo los documentos
-  },
-  ['practicas-servicio'] // Etiqueta para invalidación
-)
-
-
 export default async function ProgramasAPI() {
-  const programas = await getCachedPracticas(); // Usa la función cacheada
-  return <ServicioPracticas programas={programas} />
+  const payload = await getPayload({ config });
+  const data = await payload.find({ 
+    collection: 'practicas-servicio',
+    limit: 100,
+    sort: 'id'
+  });
+  const programas = data.docs; // Datos obtenidos
+  
+  return <ServicioPracticas programas={programas} />;
+  
 }
--- src/collections/hooks/revalidatePPySS.ts
+++ src/collections/hooks/revalidatePPySS.ts
@@ -3,12 +3,9 @@ import type { CollectionAfterChangeHook, CollectionAfterDeleteHook } from 'paylo
 import { revalidatePath } from 'next/cache';
 
 export const revalidatePracticas: CollectionAfterChangeHook = async () => {
-  console.log("Revalidando caché de practicas-servicio...");
-  revalidateTag('practicas-servicio');
   revalidatePath('/servicio-practicas'); // Cambia por la ruta real
 };
 
 export const revalidatePracticasDelete: CollectionAfterDeleteHook = async () => {
-  console.log("Revalidando caché de practicas-servicio después de eliminación")
   revalidateTag('practicas-servicio')  // Borra la caché cuando se elimina un programa
 }

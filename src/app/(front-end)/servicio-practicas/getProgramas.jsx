2b0267399c9c9431e269ba55f6024a87ff83c24b
Ricardo Farid Perez Coyote <ti44793@uvp.edu.mx>
2025-03-14 02:24:54 -0600

    Working hooks

--- src/app/(front-end)/servicio-practicas/getProgramas.jsx
+++ src/app/(front-end)/servicio-practicas/getProgramas.jsx
@@ -1,3 +1,7 @@
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import ServicioPracticas from './Client'
+
 export default async function ProgramasAPI() {
   const payload = await getPayload({ config });
   const data = await payload.find({ 
